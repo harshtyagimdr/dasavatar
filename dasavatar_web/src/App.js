4ff32f8a0b007290786a93bb0bc0eb241165f99f
harshtyagimdr <harsh.17bcs1046@abes.ac.in>
2020-11-14 16:17:01 +0530

    Web-folder-structure

--- dasavatar_web/src/App.js
+++ dasavatar_web/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Dashboard from './Components/Dashboard/Dashboard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+ render(){
+   return(
+     <BrowserRouter>
+     <div>
+     <Switch>
+            <Route exact path='/'component={Dashboard} />
+           
+          </Switch>
+     </div>
+     </BrowserRouter>
+   )
+ }
 }
 
 export default App;
