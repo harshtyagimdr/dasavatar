c01870a891e7e60672b2143a8f78639cc7a9dd3a
savagecarol <karthiksharma1411@gmail.com>
2020-11-15 04:05:09 +0530

    backend-> ml added of face detection and crowd detection

--- dasavatar_ml/ml/views.py
+++ dasavatar_ml/ml/views.py
@@ -1,3 +1,5 @@
+import urllib.request
+
 import cv2
 from rest_framework import status
 from rest_framework.response import Response
@@ -11,18 +13,23 @@ class image(APIView):
         serializer = UserSerializer(data=request.data)
         check = dict()
         if serializer.is_valid():
-            image = cv2.imread("one.jpg")
+            url = request.data['image']
+            length = len(url)
+            p = url[length - 10:] + "jpg"
+            print(url[-1:5])
+            urllib.request.urlretrieve(url, p)
+            print(p)
+            image = cv2.imread(p)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            Cascade1 = cv2.CascadeClassifier("haarcascade_frontalcatface.xml")
-
-            face = Cascade1.detectMultiScale(
+            Cascade1 = cv2.CascadeClassifier("ml/haarcascade_frontalcatface.xml")
+            faces = Cascade1.detectMultiScale(
                 gray,
                 scaleFactor=1.3,
                 minNeighbors=3,
                 minSize=(30, 30)
             )
-
+            check['status'] = True
+            check['face'] = len(faces)
             return Response({'data': check}, status=status.HTTP_200_OK)
         check['status'] = False
         return Response({'data': check}, status=status.HTTP_200_OK)
@@ -33,39 +40,39 @@ class crowd(APIView):
         serializer = CrowdSerializer(data=request.data)
         check = dict()
         if serializer.is_valid():
-            image = cv2.imread("one.jpg")
+            url = request.data['image']
+            length = len(url)
+            p = url[length - 10:] + "jpg"
+            print(url[-1:5])
+            urllib.request.urlretrieve(url, p)
+            print(p)
+            image = cv2.imread(p)
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-            Cascade1 = cv2.CascadeClassifier("haarcascade_frontalcatface.xml")
-            faces = Cascade1.detectMultiScale(
+            Cascade1 = cv2.CascadeClassifier("ml/haarcascade_frontalcatface.xml")
+            face = Cascade1.detectMultiScale(
                 gray,
                 scaleFactor=1.3,
                 minNeighbors=3,
                 minSize=(30, 30)
             )
-
-            print("[INFO] Found {} Faces.".format(len(faces)))
-
-            Cascade2 = cv2.CascadeClassifier("haarcascade_eye.xml")
-            eyes = Cascade2.detectMultiScale(
+            Cascade2 = cv2.CascadeClassifier("ml/haarcascade_eye.xml")
+            eye = Cascade2.detectMultiScale(
                 gray,
                 scaleFactor=1.3,
                 minNeighbors=3,
                 minSize=(30, 30)
             )
-
-            print("[INFO] Found {} eyes.".format(len(eyes)))
-
-            Cascade3 = cv2.CascadeClassifier("haarcascade_fullbody.xml")
+            Cascade3 = cv2.CascadeClassifier("ml/haarcascade_fullbody.xml")
             body = Cascade3.detectMultiScale(
                 gray,
                 scaleFactor=1.3,
                 minNeighbors=3,
                 minSize=(30, 30)
             )
-
-            print("[INFO] Found {} body.".format(len(body)))
-
+            check['status'] = True
+            check['body'] = len(body)
+            check['face'] = len(face)
+            check['eye'] = len(eye)
             return Response({'data': check}, status=status.HTTP_200_OK)
         check['status'] = False
         return Response({'data': check}, status=status.HTTP_200_OK)
