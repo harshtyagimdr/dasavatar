6fec29c0eac11801e93a723917c6535271475e10
harshtyagimdr <harsh.17bcs1046@abes.ac.in>
2020-11-15 19:41:25 +0530

    web-navbar-fixes

--- dasavatar_web/src/Components/Dashboard/Navbar.js
+++ dasavatar_web/src/Components/Dashboard/Navbar.js
@@ -1,24 +1,54 @@
 import React from "react";
 import {Link, NavLink} from 'react-router-dom';
 import ProfileImage from "./Profile/ProfileImage";
+import {connect} from 'react-redux'
+import {signOut} from '../../Store/actions/authActions';
+import {firestoreConnect} from 'react-redux-firebase'
+import {compose} from 'redux'
 
-const NavBar = () => {
+const NavBar = (props) => {
+    const {auth,user_details}=props;
     return (
         <div>
             <nav style={{backgroundColor: "#226089"}} className="nav-wrapper">
                 <div className="container">
                     <Link className="brand-logo left" to="/">DasAvatar</Link>
-                    <ul className="right">
+                    {auth?.uid &&<ul className="right">
                         <li className='info'><NavLink to="/profile"><i className="material-icons">info_outline</i></NavLink></li>
                         <li><NavLink to="/profile">
-                            <ProfileImage height="35" width="35" className="nav-profile circle responsive-img" /></NavLink></li>
-                        <li className='info'><NavLink to="/profile"><i className="fa fa-sign-out material-icons"></i></NavLink></li>
-                    </ul>
+                            <ProfileImage height="35" width="35" className="nav-profile circle responsive-img" imgUrl={user_details?.imgUrl} /></NavLink></li>
+                        <li className='info'><a onClick={props.signOut}><i className="fa fa-sign-out material-icons"></i></a></li>
+                    </ul>}
                 </div>
             </nav> 
         </div>
 
     )
 }
-
-export default NavBar;
+const mapStateToProps=(state)=>{
+    let users_details;
+    let id;
+    if(state?.firebase?.auth?.uid){
+        id=state?.firebase?.auth?.uid;
+        users_details=state?.firestore?.data?.user_details;
+        
+    }
+    const user=users_details?users_details[id]:null
+    return{
+    auth:state.firebase.auth,
+    user_details: user,
+    }
+  }
+const mapDispatchToProps=(dispatch)=>{
+    return{
+      signOut:()=>dispatch(signOut())
+    }
+  }
+    
+  export default compose(
+    connect(mapStateToProps,mapDispatchToProps),
+    firestoreConnect([
+      {collection:'user_details'},
+      
+    ])
+  )(NavBar);
--- dasavatar_web/src/Components/Dashboard/Profile/ProfileImage.js
+++ dasavatar_web/src/Components/Dashboard/Profile/ProfileImage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const ProfileImage = ({height, width, className}) => {
+const ProfileImage = ({height, width, className,imgUrl}) => {
+    console.log(imgUrl)
     return (
         <div>
-            <img height={height} width={width} className={className} src='http://source.unsplash.com/200x200/?dogs'></img>
+            <img height={height} width={width} className={className} src={imgUrl || 'http://source.unsplash.com/200x200/?dogs'}></img>
         </div>
     )
 }
