d594b9bb70cb136a17d1e8dd90bce966f978d01f
savagecarol <karthiksharma1411@gmail.com>
2020-11-15 14:19:21 +0530

    capp-> custom image card added on post page

--- dasavatar_app/lib/presentation/tab_pages/post.dart
+++ dasavatar_app/lib/presentation/tab_pages/post.dart
@@ -1,3 +1,4 @@
+import 'package:dasavatar_app/presentation/custom/custom_image_box.dart';
 import 'package:dasavatar_app/utils/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +16,26 @@ class _PostState extends State<Post> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-                  body: Container(
-              color: Styles.WHITE_COLOR,
-    ),
-        ),);
+      child: Scaffold(
+        body: SingleChildScrollView(
+                  child: Container(
+            color: Styles.WHITE_COLOR,
+            child: Padding(
+              padding: const EdgeInsets.all(32),
+              child: Column(
+                children: [
+                  CustomImageContainer()
+
+
+                ],
+                ),
+            ),
+            ),
+        ),
+        ),
+      );
   }
 }
+
+
+
